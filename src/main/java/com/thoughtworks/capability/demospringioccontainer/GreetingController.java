e20f909e7e91a7e82b5cd596d413f6d65b9bc46c
Yafeng Wang <yafeng.wang@wyf.local>
2020-11-08 16:30:12 +0800

    每次调用时注入

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,21 +1,28 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
+
 @RestController
+@Scope(SCOPE_SINGLETON)
 public class GreetingController {
 
-    private final GreetingService greetingService;
+    private GreetingService greetingService;
 
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
+    public GreetingController() {
+        this.greetingService = new GreetingService();
+        System.out.println("...controller");
     }
 
     @GetMapping("/greet")
     public String greet() {
+        this.greetingService = new GreetingService();
         return greetingService.sayHi();
     }
 
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,10 +1,17 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.graalvm.compiler.serviceprovider.IsolateUtil;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-@Service
-public class GreetingService {
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 
+@Service
+@Scope(SCOPE_PROTOTYPE)
+public class GreetingService {
+    public GreetingService() {
+        System.out.println("...service");
+    }
     String sayHi() {
         return "hello world";
     }
